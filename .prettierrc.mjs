cd2d531521526e92e5253d6e2833ff6bc7631a90
HitsujiRere <hitsujirere@gmail.com>
2026-01-27 18:03:47 +0900

    feat: TailwindCSSの追加

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,7 +2,11 @@
 
 /** @type {import("prettier").Options} */
 export default {
-  plugins: ["prettier-plugin-astro", "@ianvs/prettier-plugin-sort-imports"],
+  plugins: [
+    "prettier-plugin-astro",
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
   overrides: [
     {
       files: "*.astro",
